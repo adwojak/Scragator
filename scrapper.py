32296c907faf916d1f1932405fa3a4911dd23091
Artur Dwojak <dwojaka207@gmail.com>
2019-08-31 13:36:08 +0200

    Changed parser type from html.parser to lxml

--- scrapper.py
+++ scrapper.py
@@ -6,7 +6,9 @@ from requests import get as rget
 
 class Scrapper(object):
 
-    def __init__(self, url, parser_type='html.parser'):
+    default_parser = 'lxml'
+
+    def __init__(self, url, parser_type=default_parser):
         self.url = url
         self.main_soup = self.get_soup_from_link(url, parser_type)
         self.articles = self.get_articles()
@@ -20,6 +22,6 @@ class Scrapper(object):
     def get_response_from_link(self, url):
         return rget(url)
 
-    def get_soup_from_link(self, url, parser_type='html.parser'):
+    def get_soup_from_link(self, url, parser_type=default_parser):
         response = self.get_response_from_link(url)
         return BeautifulSoup(response.text, parser_type)
