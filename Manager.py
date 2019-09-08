90b63db5e131f4019c1e7ee213503ee74635720b
Artur Dwojak <dwojaka207@gmail.com>
2019-09-08 11:41:35 +0200

    Manager

---
+++ Manager.py
@@ -0,0 +1,37 @@
+from libs.Observer.observer import BaseObserver
+from niebezpiecznik import Niebezpiecznik
+
+
+class NiebezpiecznikObserver(BaseObserver):
+
+    def check_for_posts_updates(self):
+        niebezpiecznik = Niebezpiecznik(self.get_url())
+        main_site_articles = niebezpiecznik.get_main_site_articles()
+        # comparing compare_hash with one saved in db
+        # if there is something new, create ArticleModel from CompareArticleModel
+        # add to db and update latest hashed article in db
+        new_main_site_articles = self.get_new_posts(main_site_articles)
+        # create article from base article
+        # self.create_article_model_from_compare_article()
+
+        # import pdb;pdb.set_trace()
+
+    def get_new_posts(self, articles):
+        # get latest hash from db and get newest
+        # for testing purpose, last 3 posts are new
+        return articles[:3]
+
+
+class Manager(object):
+
+    observers_dict = {
+        'http://www.niebezpiecznik.pl': NiebezpiecznikObserver
+    }
+
+    @property
+    def _observers_list(self):
+        return [observer(url) for url, observer in self.observers_dict.items()]
+
+    def execute_observers(self):
+        for observer in self._observers_list:
+            observer.check_for_posts_updates()
