02de191d43f26d204acb67d46a8f3bf7e64fd367
Artur Dwojak <dwojaka207@gmail.com>
2019-12-16 21:57:22 +0100

    Refactor of observers mapping

--- backend/observers_mapping.py
+++ backend/observers_mapping.py
@@ -3,10 +3,12 @@ from attrdict import AttrDict
 from backend.scrappers.names import ScrapperNames
 from backend.scrappers.niebezpiecznik import Niebezpiecznik
 
-observers_mapping: list = [
-    AttrDict({
-        'name': ScrapperNames.NIEBEZPIECZNIK.value,
-        'url': 'http://www.niebezpiecznik.pl/',
-        'scrapper': Niebezpiecznik
-    })
+observers_elements: list = [
+    [ScrapperNames.NIEBEZPIECZNIK.value, 'http://www.niebezpiecznik.pl/', Niebezpiecznik],
 ]
+
+observers_mapping: list = [AttrDict(
+    name=value[0],
+    url=value[1],
+    scrapper=value[2]
+) for value in observers_elements]
