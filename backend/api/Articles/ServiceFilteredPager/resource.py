2d6eb58b5b94e153ff5bcac04eb1252b73a6f85e
Artur Dwojak <dwojaka207@gmail.com>
2019-12-16 22:45:34 +0100

    Services filtration completed

--- backend/api/Articles/ServiceFilteredPager/resource.py
+++ backend/api/Articles/ServiceFilteredPager/resource.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 from flask import Response, jsonify
 from flask_restful import Resource, request
 from werkzeug.exceptions import NotFound
@@ -8,15 +9,16 @@ from backend.models.models import ArticleModel
 class FilteredPager(Resource):
 
     def get(self) -> Response:
-        page: int = int(request.args.get('page'))
-        # services: list = list(request.args.get('service'))
+        page_str: str = request.form.get('page')
+        services_str: str = request.form.get('services')
 
-        # try:
-            # filtered_models = ArticleModel.query.order_by(ArticleModel.id.desc()).filter(
-            #     ArticleModel.name.in_(services)).paginate(page=page, per_page=8).items
-            # return jsonify([article.get_article() for article in filtered_models])
-        # except NotFound:
-        #     return jsonify([])
-        # except TypeError:
-        #     return jsonify([{'elo': 12}])
-        return jsonify([])
+        try:
+            page = int(page_str)
+            services = literal_eval(services_str)
+            filtered_models = ArticleModel.query.order_by(ArticleModel.id.desc()).filter(
+                ArticleModel.name.in_(services)).paginate(page=page, per_page=8).items
+            return jsonify([article.get_article() for article in filtered_models])
+        except NotFound:
+            return jsonify([])
+        except TypeError:
+            return jsonify([])
