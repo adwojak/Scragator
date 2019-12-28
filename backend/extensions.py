ca5981876687930fb30a359f3ca92de151a9cfb0
Artur Dwojak <dwojaka207@gmail.com>
2019-12-28 12:48:17 +0100

    Updates

--- backend/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_apscheduler import APScheduler
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-db: SQLAlchemy = SQLAlchemy()
-migrate: Migrate = Migrate()
-scheduler: APScheduler = APScheduler()
--- backend/scrappers/names.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ScrapperNames(Enum):
-    NIEBEZPIECZNIK: str = 'Niebezpiecznik'
-
-    @classmethod
-    def list_values(cls) -> list:
-        return list(map(lambda el: el.value, cls))
