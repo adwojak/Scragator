667dda54bd6eedad23ff93a4e75344efbc744130
Artur Dwojak <dwojaka207@gmail.com>
2019-12-28 12:51:54 +0100

    Updates

---
+++ precache-manifest.86d7bb1893908038e55724ebd9874faa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25893aff8dce4c6855954125879ac7d",
+    "url": "/Scragator/index.html"
+  },
+  {
+    "revision": "8df831d2bd6d70245d1a",
+    "url": "/Scragator/static/css/main.5ce54d3b.chunk.css"
+  },
+  {
+    "revision": "ee4d30a2cdecc37dba28",
+    "url": "/Scragator/static/js/2.a05410d0.chunk.js"
+  },
+  {
+    "revision": "8df831d2bd6d70245d1a",
+    "url": "/Scragator/static/js/main.9749a7dc.chunk.js"
+  },
+  {
+    "revision": "557ae1d19a858320a05d",
+    "url": "/Scragator/static/js/runtime-main.0f3492c3.js"
+  },
+  {
+    "revision": "8fcd1d9acdd54fb5155d0f93f5a99da7",
+    "url": "/Scragator/static/media/backgroundImage.8fcd1d9a.jpg"
+  },
+  {
+    "revision": "5ce58a958c9088c156980974242d4b8b",
+    "url": "/Scragator/static/media/houseIcon.5ce58a95.jpg"
+  }
+]);
