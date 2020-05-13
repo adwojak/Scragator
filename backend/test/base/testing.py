abb54eb8d63b487b70f63917f995601f83c2438d
Artur Dwojak <dwojaka207@gmail.com>
2020-05-13 18:54:54 +0200

    Added testing base for resource

--- backend/test/base/testing.py
+++ backend/test/base/testing.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.testing import FlaskClient
+from flask.wrappers import Response
 from json import dumps
-from typing import NoReturn
+from typing import NoReturn, Optional
 
 from backend.config import TestingConfig
 from backend.test.base.database import DatabaseBase
@@ -17,3 +19,29 @@ class TestingBase(DatabaseBase):
         dict_dumped: str = dumps(dictionary, sort_keys=True, default=str)
         excpected_dumped: str = dumps(expected, sort_keys=True, default=str)
         assert dict_dumped == excpected_dumped
+
+
+class ResourceTesting(TestingBase):
+
+    default_headers: dict = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    @property
+    def url(self) -> NoReturn:
+        raise NotImplementedError
+
+    def client(self) -> FlaskClient:
+        return self.app.test_client()
+
+    def get(self, headers: Optional[dict] = None) -> Response:
+        return self.client().get(self.url, headers=headers or self.default_headers)
+
+    def post(self, request: dict, headers: Optional[dict] = None) -> Response:
+        return self.client().post(self.url, headers=headers or self.default_headers, data=request)
+
+    def request_get(self, headers: Optional[dict] = None) -> list:
+        return self.get(headers).json
+
+    def request_post(self, request: dict, headers: Optional[dict] = None) -> list:
+        return self.post(request, headers).json
